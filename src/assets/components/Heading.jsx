ffe558e139a2269adb4fd350c67b4a22ab8bd871
jnoyon <jihadur51@gmail.com>
2026-06-11 17:36:51 +0600

    First Commit

---
+++ src/assets/components/Heading.jsx
@@ -0,0 +1,7 @@
+export default function Heading({ text }) {
+  return (
+    <div class="w-full max-w-[472.55px] text-center flex justify-center flex-col text-[#49423D] text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold leading-tight md:leading-[60px] tracking-tight">
+      {text}
+    </div>
+  );
+}
